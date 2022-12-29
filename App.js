c085ab5d4d38274d123e2db99afe53d75f31fdde
cathy-quiza <cathyquiza@student.laverdad.edu.ph>
2022-12-29 14:54:43 +0800

    change text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up! Testing</Text>
+      <Text>Open up! Testing! Testing!</Text>
       <StatusBar style="auto" />
     </View>
   );
